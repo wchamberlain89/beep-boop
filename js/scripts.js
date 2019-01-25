636be946d023e9d11c89de6a28f34d0605fa565b
ward chamberlain <wchamberlain89@gmail.com>
2019-01-25 08:51:40 -0800

    it will replace numbers containing 1 with beep and 2 with boop

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 function testForChange(num) {
 	var splitNumber = num.toString().split('');
 	if(splitNumber.includes('3')) {
-		return "I'm sorry Dave, I'm afraid I can't do that."
+		return "I'm sorry Dave, I'm afraid I can't do that.";
+	} else if(splitNumber.includes('2')) {
+		return "Boop";
+	} else if(splitNumber.includes('1')) {
+		return "Beep";
 	} else {
 		return num;
 	}
