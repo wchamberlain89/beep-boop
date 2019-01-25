b4ef1599855fb0a956b00b40773f14c8781a3934
ward chamberlain <wchamberlain89@gmail.com>
2019-01-25 12:08:27 -0800

    result divs will connect together

--- js/scripts.js
+++ js/scripts.js
@@ -20,17 +20,34 @@ function beepBoop(numberString) {
 	return numberArray;
 }
 
+function displayNext(currentIndex, length) {
+	if(currentIndex <= length) {
+		$("#" + currentIndex).addClass("animated fadeInUp").show();
+		var currentDisplayResult = document.getElementById(currentIndex);
+		currentDisplayResult.addEventListener("animationend", function() {
+			displayNext(currentIndex + 1, length);
 
-
-
+		});
+	}
+}	
 
 $(function(){
 	$("button").click(function(){
+		
 		var userNumber = $("#user-number").val();
+		
 		results = beepBoop(userNumber);
-		results.forEach(function(result){
-			$(".result-container").append("<div class='result'><span>" + result + "</span></div>");
+		
+		results.forEach(function(result, index){
+				$(".result-container").append("<div class='result col-md-2' id='" + index + "'><span>" + result + "</span></div>");
 		});
-		$(".result").addClass("col-md-2 m-2");
+
+		displayNext(0, results.length);
+
+			
+		
+			
+		
+		
 	});
 });
