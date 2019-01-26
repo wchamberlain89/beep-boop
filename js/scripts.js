446299e20887c7db4d49682dba5c061b98a85943
ward chamberlain <wchamberlain89@gmail.com>
2019-01-25 16:28:42 -0800

    filter is removed to fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function testForChange(num) {
 	var splitNumber = num.toString().split('');
 	if(splitNumber.includes('3')) {
-		return "I'm sorry Dave, I'm afraid I can't do that";
+		return "Blip";
 	} else if(splitNumber.includes('2')) {
 		return "Boop";
 	} else if(splitNumber.includes('1')) {
@@ -21,33 +21,92 @@ function beepBoop(numberString) {
 }
 
 function displayNext(currentIndex, length) {
-	if(currentIndex <= length) {
-		$("#" + currentIndex).addClass("animated fadeInUp").show();
+	var hexArray = ['#0D1940','#F2B705','#D9C5A0', '#F29F05', '#8C4E03'];
+	var randomColor = hexArray[Math.floor(Math.random() * hexArray.length)];
+
+	if(currentIndex < length) {
+
+		$("#" + currentIndex).addClass("animated fadeInUp").css("background-color", randomColor).show();
 		var currentDisplayResult = document.getElementById(currentIndex);
 		currentDisplayResult.addEventListener("animationend", function() {
 			displayNext(currentIndex + 1, length);
-
 		});
+
+	} else {
+		
+		$(".reset-container").show();
+
+	};
+
+}
+
+function deleteResults(index) {
+	$(".reset-container").hide();
+
+	if (index >= 0) {
+		$("#" + index).removeClass("fadeInUp");	
+		$("#" + index).addClass("fadeOutLeft");
+		var currentResult = document.getElementById(index);
+		
+		currentResult.addEventListener("animationend", function() {
+			deleteResults(index - 1);
+		});
+	} else {
+		$("#submit").show();
+		$(".result").remove();
 	}
-}	
+
+}
+
+function restart() {
+	deleteResults($(".result").length - 1);
+	$("input").val("");
+}
+
+
+
 
 $(function(){
-	$("button").click(function(){
-		
+	$("button#submit").click(function(){
+
 		var userNumber = $("#user-number").val();
 		
-		results = beepBoop(userNumber);
-		
-		results.forEach(function(result, index){
+		if(userNumber > 0) {
+			
+			var results = beepBoop(userNumber);
+
+			results.forEach(function(result, index){
 				$(".result-container").append("<div class='result col-md-2' id='" + index + "'><span>" + result + "</span></div>");
-		});
-
-		displayNext(0, results.length);
-
+			});
 			
-		
+			$("#submit").hide();
 			
-		
-		
+			displayNext(0, results.length);
+		}
+
 	});
+
+	$("button#reset").click(function(){
+		$(".reset-container").hide();
+		restart();
+
+	});
+
+	$("button#filter").click(function(){
+		var filters = []
+		$(".result").hide();
+		$("input[type='checkbox'][name='filter']:checked").each(function(filter){
+			var currentFilter = $(this).val();
+			filters.push(currentFilter);
+		});
+		
+		if(filters.length === 0) {
+			$(".result").show();
+		};
+
+		filters.forEach(function(filter){
+			  $(".result:contains('" + filter + "')").show();
+		});
+	});
+
 });
